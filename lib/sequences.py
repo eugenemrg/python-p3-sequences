f70f25ba371a438871180e4d7c6ebf6ff1a53f89
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-08-20 14:54:11 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = []
+    for i in range(length):
+        if i < 2:
+            fib.append(i)
+        else:
+            new_num = fib[len(fib)-1] + fib[len(fib)-2]
+            fib.append(new_num)
+    print(fib)
+    
+# print_fibonacci(0)
